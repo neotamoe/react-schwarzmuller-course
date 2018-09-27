e5822819198b5e3354f96fece56260d5a69b8467
Neota Moe <neota.moe@gmail.com>
2018-09-26 23:50:03 -0500

    props, props.children, state, handlers

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: "Eli", age: 10},
+      {name: "Sam", age: 8},
+      {name: "Violet", age: 3},
+    ],
+    otherState: "oatmeal"
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked!')
+    this.setState({persons: [
+      {name: "Eli", age: 11},
+      {name: "Samuel", age: 8},
+      {name: "Violet", age: 3},
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Soccer</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
