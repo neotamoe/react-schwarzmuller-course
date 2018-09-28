ea44042a2a3e86cc802c9386b930cb36a16837d3
Neota Moe <neota.moe@gmail.com>
2018-09-27 19:55:00 -0500

    inline and global styling

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = (props) => {
     return (
-        <div onClick={props.click}>
+        <div className="Person" onClick={props.click}>
             <p>My name is {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
