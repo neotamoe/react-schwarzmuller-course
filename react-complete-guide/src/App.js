4d3e1e6c228b6f6f7317ac700e09d04a7b674d03
Neota Moe <neota.moe@gmail.com>
2018-09-29 17:54:11 -0500

    removing radium before npm eject

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import './Person/Person.css';
-import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
 
@@ -54,10 +53,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
@@ -76,10 +71,6 @@ class App extends Component {
         </div> 
       )
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     let classes = [];
@@ -92,18 +83,16 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React app!</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button 
-            style={style} 
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React app!</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button 
+          style={style} 
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
