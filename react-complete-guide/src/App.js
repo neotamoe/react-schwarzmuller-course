761f5615f84e69771c9d36bc4e637eb778943405
Neota Moe <neota.moe@gmail.com>
2018-09-29 18:45:19 -0500

    re-adding pseudoselectors using css modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -46,16 +46,8 @@ class App extends Component {
 
   render() {
 
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if ( this.state.showPersons ) {
       persons = (
@@ -69,8 +61,10 @@ class App extends Component {
               changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div> 
-      )
-      style.backgroundColor = 'red';
+      );
+
+      btnClass = classes.Red;
+
     }
 
     let assignedClasses = [];
@@ -87,7 +81,7 @@ class App extends Component {
         <h1>Hi, I'm a React app!</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
-          style={style} 
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
