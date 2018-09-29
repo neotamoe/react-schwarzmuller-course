29ff51c817744b6bcc15df763dd78725f233d9c0
Neota Moe <neota.moe@gmail.com>
2018-09-28 22:04:59 -0500

    media queries using radium's StyleRoot

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import './Person/Person.css';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
 
@@ -92,14 +92,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React app!</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style} 
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React app!</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style} 
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
