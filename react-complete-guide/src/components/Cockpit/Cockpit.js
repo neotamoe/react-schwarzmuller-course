f15549c7ad1256105dd98754d8d7675393e5791a
Neota Moe <neota.moe@gmail.com>
2018-09-30 12:08:36 -0500

    refactor for example of using a higher order component

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import classes from './Cockpit.css'
+import classes from './Cockpit.css';
+import Aux from '../hoc/Aux';
 
 const cockpit = (props) => {
     let assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
     if(props.showPersons){
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
 
     if (props.persons.length <= 2 ) {
@@ -16,13 +17,13 @@ const cockpit = (props) => {
     }
 
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>Hi, I'm a React app!</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button 
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
-        </div>
+        </Aux>
     );
 }
 
