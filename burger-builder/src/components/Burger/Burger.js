0297fc484b030cb0b6ff1eac30a50a4d8839a0c0
Neota Moe <neota.moe@gmail.com>
2018-10-04 18:28:49 -0500

    reduce array of ingredients and start to add buildcontrols and buildcontrol components

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -5,12 +5,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     // some fancy javascript to turn the state object into an array of burger ingredients
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])].map((__, i) => {
                 return <BurgerIngredient key={ingKey + i} type={ingKey} />
             })
         })
+        .reduce((arr, el)=>{
+            return arr.concat(el)
+        }, []);
+
+    if(transformedIngredients.length===0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
 
     return (
         <div className={classes.Burger}>
