95c7dde604cc4b24bf09f68a33171bcd4b567c99
Neota Moe <neota.moe@gmail.com>
2018-10-07 13:49:15 -0500

    toggling side drawer open and shut

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Toggle from '../SideDrawer/Toggle/Toggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <Toggle clicked={props.toggleClicked}/>
         <div className={classes.Logo}>
             <Logo />
         </div>
