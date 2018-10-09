1a765ae6c20fb1e68dbdb4540b1fc7262bfc6897
Neota Moe <neota.moe@gmail.com>
2018-10-08 19:15:54 -0500

    checking for optimization via component lifecycle methods

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
+class Modal extends Component {
 
+    componentWillUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show? '1' : 0
-            }} 
-            className={classes.Modal}>
-            {props.children}
-        </div>
-    </Aux>
-);
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={props.show} clicked={props.modalClosed}/>
+                <div
+                    style={{
+                        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: props.show? '1' : 0
+                    }} 
+                    className={classes.Modal}>
+                    {props.children}
+                </div>
+            </Aux>
+        );
+    }
+};
 
-export default modal;
+export default Modal;
