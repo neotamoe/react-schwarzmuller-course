a212de0299af1fcdb46deb37d8c47d32a7eed2ed
Neota Moe <neota.moe@gmail.com>
2018-10-10 19:30:43 -0500

    refactoring for using global default configs in axios

--- http-axios/src/index.js
+++ http-axios/src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTHTOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
     console.log(request);
     // could edit request config here
