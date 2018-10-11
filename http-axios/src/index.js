a7046150cf4cfab149ed4d5e00b3d2df69f89ca3
Neota Moe <neota.moe@gmail.com>
2018-10-10 19:25:37 -0500

    axios interceptors

--- http-axios/src/index.js
+++ http-axios/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // could edit request config here
+    // common use here would be as a way to add a common headers (such as for authorization)
+    return request;  // without this line, your interceptor is blocking the request
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // could edit response config here
+    return response;  // without this line, your interceptor is blocking the request
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
