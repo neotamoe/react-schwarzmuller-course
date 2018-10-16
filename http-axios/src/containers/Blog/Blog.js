7e33b04bd4017576eb9b102100e434e86c2a144b
Neota Moe <neota.moe@gmail.com>
2018-10-16 18:51:42 -0500

    using navlink instead of link to get class='active' automatically applied to active navlink

--- http-axios/src/containers/Blog/Blog.js
+++ http-axios/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
@@ -15,14 +15,17 @@ class Blog extends Component {
                 <header >
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: '/new-post',  // this always creates an absolute path
-                                // to create relative path and append new-post to end of page/link you're on:
-                                // use to={props.match.url + '/new-post}
-                                hash: '#submit',  // these last two aren't used, but are examples
-                                search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            <li><NavLink 
+                                exact 
+                                to="/" 
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: 'purple',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
