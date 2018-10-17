fb9fcfd148750d43f48d2b42c8f6166c29804994
Neota Moe <neota.moe@gmail.com>
2018-10-16 19:26:32 -0500

    using switch to restrict routing and using params for routes

--- http-axios/src/containers/Blog/Blog.js
+++ http-axios/src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
+import FullPost from '../FullPost/FullPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -31,7 +32,10 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
                 <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
+                <Switch>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
             </div>
         );
     }
--- http-axios/src/containers/Posts/Posts.js
+++ http-axios/src/containers/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {Link} from 'react-router-dom';
 import axios from '../../axios';
 import Post from '../../components/Post/Post';
 import './Posts.css';
@@ -40,11 +40,13 @@ class Posts extends Component {
         let posts = <p style={{textAlign: 'center'}}>Oops!  Something went wrong.</p>
         if(!this.state.error){
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    clicked={() => this.postSelectedHandler(post.id)} 
-                    title={post.title} 
-                    author={post.author}/>
+                return (
+                <Link to={'/' + post.id} key={post.id} >
+                    <Post 
+                        clicked={() => this.postSelectedHandler(post.id)} 
+                        title={post.title} 
+                        author={post.author}/>
+                </Link>);
             })
         }
 
