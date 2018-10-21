393bf8ba6a8b3fdfbb21198e6bb08f55015df779
Neota Moe <neota.moe@gmail.com>
2018-10-21 11:17:33 -0500

    outputting the orders

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -22,7 +22,7 @@ class ContactData extends Component {
         this.setState({loading: true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.totalPrice,  // in prod app calculate price on server
+            price: this.props.price, 
             customer: {
                 name: 'Neota Moe',
                 address: {
