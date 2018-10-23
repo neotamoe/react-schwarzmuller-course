33d6eb839d81a633f2d088d3826ccbdc99be2d51
Neota Moe <neota.moe@gmail.com>
2018-10-22 19:02:44 -0500

    submitting form

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -64,11 +64,16 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
-        console.log('this.props.ingredients in orderhandler ',this.props.ingredients);  // passed due to using render instead of component in <Route>
         this.setState({loading: true});
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+        console.log(formData);
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price, 
+            orderData: formData,
         }
         axios.post('/orders.json', order) //node_name.json is format for firebase
         .then(response => {
@@ -103,7 +108,7 @@ class ContactData extends Component {
             })
         }
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input 
                         key={formElement.id}
@@ -112,7 +117,7 @@ class ContactData extends Component {
                         value={formElement.config.value}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success" >ORDER</Button>
             </form>
         );
         if(this.state.loading){
