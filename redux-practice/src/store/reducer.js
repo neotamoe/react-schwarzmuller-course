4ebef761d148ee7977e7930fd44a2375c707ecd9
Neota Moe <neota.moe@gmail.com>
2018-10-27 13:43:39 -0500

    practice converting to using redux

---
+++ redux-practice/src/store/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+    persons: [],
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_PERSON':
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            };
+            return {
+                ...state,
+                persons: state.persons.concat(newPerson),  // concat creates a new array (push does not)
+            }
+        case 'DELETE_PERSON':
+            // filter creates a new array
+            const updatedArray = state.persons.filter(person => person.id !== action.personId);
+            return {
+                ...state,
+                persons: updatedArray,
+            }
+    }
+
+    return state;
+};
+
+export default reducer;
