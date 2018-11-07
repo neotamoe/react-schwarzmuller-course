c2a89689ba30f1b4724d8d4e39baac2f64bba799
Neota Moe <neota.moe@gmail.com>
2018-11-06 18:30:05 -0600

    converting to using actions for order

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
     state = {
@@ -109,25 +111,16 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
-        this.setState({loading: true});
         const formData = {};
         for(let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
-        console.log(formData);
         const order = {
             ingredients: this.props.ingts,
             price: this.props.ttlPrice, 
             orderData: formData,
-        }
-        axios.post('/orders.json', order) //node_name.json is format for firebase
-        .then(response => {
-            this.setState({loading: false});
-            this.props.history.push('/');
-        })
-        .catch(error => {
-            this.setState({loading: false});
-        });  
+        } 
+        this.props.onOrderBurger(order);
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
@@ -197,4 +190,8 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData));
+}
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
--- burger-builder/src/store/actions/index.js
+++ burger-builder/src/store/actions/index.js
@@ -6,5 +6,5 @@ export {
 } from './burgerBuilder';
 
 export {
-
+    purchaseBurgerStart
 } from './order';
