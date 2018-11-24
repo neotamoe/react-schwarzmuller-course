9a2fecd02a1ea59052a70e5373b89fed9bbf2d1b
Neota Moe <neota.moe@gmail.com>
2018-11-24 17:33:57 -0600

    one more test

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -10,15 +10,23 @@ configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
     let wrapper;
+    
     beforeEach(()=> {
         wrapper = shallow(<NavigationItems />);
     });
+    
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        expect(wrapper.find(NavigationItem)).toHaveLength(2)
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
+
     it('should render three <NavigationItem /> elements if authenticated', () => {
         // wrapper = shallow(<NavigationItems isAuthenticated/>);  // alternate to using setProps
         wrapper.setProps({isAuthenticated: true});
-        expect(wrapper.find(NavigationItem)).toHaveLength(3)
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render a logout button if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
