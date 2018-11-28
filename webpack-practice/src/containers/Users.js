d28e5896e778810c78c31a577ad855c6286d5a46
Neota Moe <neota.moe@gmail.com>
2018-11-27 19:27:47 -0600

    adding files and structure like create-react-app for better understanding of webpack

---
+++ webpack-practice/src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Pizza extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users!!!</h1>
+                <p>The best users are here.</p>
+            </div>
+        );
+    }
+}
+
+export default Pizza;
