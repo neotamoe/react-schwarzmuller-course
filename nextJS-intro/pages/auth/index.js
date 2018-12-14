9feef1bab305bcb68e8b93ea6f9c55ad4abe6240
Neota Moe <neota.moe@gmail.com>
2018-12-13 22:37:55 -0600

    using a traditional react component with next

--- nextJS-intro/pages/auth/index.js
+++ nextJS-intro/pages/auth/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Router from 'next/router';
 
+import User from '../../components/User';
+
 const authIndexPage = () => {
     return (
         <div>
             <h1>The Auth Index Page</h1>
+            <User name={Violet} age={3}/>
             <button onClick={() => Router.push('/')}>Go to Main</button>
         </div>
     )
