7439b71ffdca550db382d8a4d2a62bcb53671244
Neota Moe <neota.moe@gmail.com>
2018-12-17 21:36:28 -0600

    using getInitialProps lifecycle hook to prepopulate page with props

--- nextJS-intro/pages/auth/index.js
+++ nextJS-intro/pages/auth/index.js
@@ -3,14 +3,23 @@ import Router from 'next/router';
 
 import User from '../../components/User';
 
-const authIndexPage = () => {
+const authIndexPage = (props) => {
     return (
         <div>
-            <h1>The Auth Index Page</h1>
+            <h1>The Auth Index Page - {props.appName}</h1>
             <User name="Violet" age="3"/>
             <button onClick={() => Router.push('/')}>Go to Main</button>
         </div>
     )
+};
+
+authIndexPage.getInitialProps = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: 'Super App (Auth)'});
+        },1000);
+    });
+    return promise;
 };
 
 export default authIndexPage;
--- nextJS-intro/pages/index.js
+++ nextJS-intro/pages/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
 
-const indexPage = () => {
-    return(
-        <div>
-            <h1>The MAIN Page</h1>
-            <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        </div>
-    );
+class IndexPage extends Component {
+    // special lifecycle hook to prepopulate props
+    static getInitialProps(context){
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({ appName: 'Super App'});
+            },1000);
+        });
+        return promise;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The MAIN Page of {this.props.appName}</h1>
+                <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+                <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+            </div>
+        );
+    }    
 };
 
-export default indexPage;
+export default IndexPage;
