b5a6ff102da628daa5162f2e0deff8d65f84117f
Neota Moe <neota.moe@gmail.com>
2018-12-23 22:42:49 -0600

    fixing typos

--- burger-builder/src/store/sagas/index.js
+++ burger-builder/src/store/sagas/index.js
@@ -3,6 +3,6 @@ import { takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga } from './auth';
 
-function* watchAuth(){
-    yield takeEvery(actionTypes.AUT_INITIATE_LOGOUT, logoutSaga);
+export function* watchAuth(){
+    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
 }
