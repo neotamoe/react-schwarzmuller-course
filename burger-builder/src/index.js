4180c83ab8c53a5b16d92e20f96ac276388c9241
Neota Moe <neota.moe@gmail.com>
2018-12-25 22:47:20 -0600

    converting actions with side effects in order to use saga

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -11,7 +11,7 @@ import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
-import { watchAuth, watchBurgerBuilder } from './store/sagas';
+import { watchAuth, watchBurgerBuilder, watchOrder } from './store/sagas';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
@@ -29,6 +29,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 sagaMiddleware.run(watchAuth);
 sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
 
 const app = (
     <Provider store={store}>
--- burger-builder/src/store/actions/order.js
+++ burger-builder/src/store/actions/order.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
+import order from '../../components/Order/Order';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
@@ -23,16 +24,11 @@ export const purchaseBurgerStart = () => {
 }
 
 export const purchaseBurger = (orderData, token) => {
-    return dispatch => {
-        dispatch(purchaseBurgerStart());
-        axios.post('/orders.json?auth=' + token, orderData) 
-        .then(response => {
-            dispatch(purchaseBurgerSuccess(response.data.name, orderData))
-        })
-        .catch(error => {
-            dispatch(purchaseBurgerFail(error))
-        }); 
-    };
+    return {
+        type: actionTypes.PURCHASE_BURGER,
+        orderData: orderData,
+        token: token
+    }
 };
 
 export const purchaseInit = () => {
@@ -62,22 +58,9 @@ export const fetchOrdersStart = () => {
 }
 
 export const fetchOrders = (token, userId) => {
-    return dispatch => {
-        dispatch(fetchOrdersStart());
-        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
-        axios.get('/orders.json' + queryParams)
-        .then(response => {
-            const fetchedOrders = [];
-            for(let key in response.data) {
-                fetchedOrders.push({
-                    ...response.data[key],
-                    id: key
-                });
-            }
-            dispatch(fetchOrdersSuccess(fetchedOrders));
-        })
-        .catch(error => {
-            dispatch(fetchOrdersFail(error));
-        });
+    return {
+        type: actionTypes.FETCH_ORDERS,
+        token: token,
+        userId: userId
     }
 }
