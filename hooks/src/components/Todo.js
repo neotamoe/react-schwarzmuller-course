5e101eec2240fdab263253b2eb0ca87570fbe8a3
Neota Moe <neota.moe@gmail.com>
2018-12-27 18:55:35 -0600

    rewritten using js array destructuring

--- hooks/src/components/Todo.js
+++ hooks/src/components/Todo.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 const todo = props => {
     // useState returns an array with two elements, 
     // [0]: current/latest state, [1]: a reference to a function to change state
-    const inputState = useState('');
+    const [todoName, setTodoName] = useState('');
     
     const inputChangeHandler = (event) => {
-        inputState[1](event.target.value)
+        setTodoName(event.target.value)
     };
 
     return (
@@ -15,7 +15,7 @@ const todo = props => {
                 type="text" 
                 placeholder="To Do"
                 onChange={inputChangeHandler}
-                value={inputState[0]} />
+                value={todoName} />
             <button type="button">Add</button>
             <ul />
         </React.Fragment>
