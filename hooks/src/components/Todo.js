12010bdc10b720ff1dea3e4c54b104882b921d83
Neota Moe <neota.moe@gmail.com>
2018-12-27 19:02:09 -0600

    using multiple states with useState hook

--- hooks/src/components/Todo.js
+++ hooks/src/components/Todo.js
@@ -4,10 +4,16 @@ const todo = props => {
     // useState returns an array with two elements, 
     // [0]: current/latest state, [1]: a reference to a function to change state
     const [todoName, setTodoName] = useState('');
+
+    const [todoList, setTodoList] = useState([]);
     
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value)
     };
+
+    const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName))
+    }
 
     return (
         <React.Fragment>
@@ -16,8 +22,12 @@ const todo = props => {
                 placeholder="To Do"
                 onChange={inputChangeHandler}
                 value={todoName} />
-            <button type="button">Add</button>
-            <ul />
+            <button type="button" onClick={todoAddHandler}>Add</button>
+            <ul>
+                {todoList.map(todo=> (
+                    <li key={todo}>{todo}</li>
+                ))}
+            </ul>
         </React.Fragment>
     ); 
 };
